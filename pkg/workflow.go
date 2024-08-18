302b0676eb0ce417612b765578472d7f38d6fe52
cembdc <cembdci@gmail.com>
2024-08-18 10:52:15 +0300

    Refactor workflow sorting method to update steps in order
    Modified the `sortSteps` method of `Workflow` to update steps in a sorted order based on keys. This refactor ensures that steps are now updated sequentially according to their sorted keys, aligning with the intended workflow processing order.

--- pkg/workflow.go
+++ pkg/workflow.go
@@ -1,6 +1,9 @@
 package workflow
 
-import "log"
+import (
+	"log"
+	"sort"
+)
 
 type LocalTransaction func() error
 type CompensatingAction func() error
@@ -25,22 +28,48 @@ func (w *Workflow) AddStep(name string, step SagaStep) {
 }
 
 func (w *Workflow) Execute() error {
+	w.sortSteps()
+
+	var completedSteps []string
+
 	for stepName, stepFunc := range w.Steps {
 		log.Printf("[Executing step] %s", stepName)
 
 		if err := stepFunc.Transaction(); err != nil {
 			log.Printf("[Error executing step] %s: %s", stepName, err)
 
-			for n, f := range w.Steps {
+			// Perform compensating actions for all previously completed steps
+			for i := len(completedSteps) - 1; i >= 0; i-- {
+				n := completedSteps[i]
 				log.Printf("[Compensating step] %s", n)
-				f.Compensate()
-
-				if stepName == n {
-					break
+				if err := w.Steps[n].Compensate(); err != nil {
+					log.Printf("[Error compensating step] %s: %s", n, err)
+					// Handle compensation error if needed
 				}
 			}
+
+			// Stop execution as soon as an error occurs
+			return err
 		}
+
+		// Track successfully completed steps
+		completedSteps = append(completedSteps, stepName)
 	}
 
 	return nil
+}
+
+func (w *Workflow) sortSteps() {
+	keys := make([]string, 0, len(w.Steps))
+	for k := range w.Steps {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	sortedSteps := make(map[string]SagaStep, len(w.Steps))
+	for _, k := range keys {
+		sortedSteps[k] = w.Steps[k]
+	}
+
+	w.Steps = sortedSteps
 }
